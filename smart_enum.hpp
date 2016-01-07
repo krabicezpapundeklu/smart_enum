f602b6dfab4f53877c2b345dffec983b9dd479a6
JF <jarda@jarda.info>
2016-01-07 09:48:13 +0100

    Clean macros.

--- smart_enum.hpp
+++ smart_enum.hpp
@@ -7,6 +7,7 @@
 #include <boost/preprocessor/stringize.hpp>
 #include <boost/preprocessor/tuple/insert.hpp>
 #include <boost/preprocessor/tuple/pop_front.hpp>
+#include <boost/preprocessor/tuple/replace.hpp>
 #include <boost/preprocessor/tuple/to_seq.hpp>
 #include <boost/vmd/is_tuple.hpp>
 
@@ -17,6 +18,9 @@
     SMART_ENUM_IMPL(class, NAME, VALUES)
 
 #define SMART_ENUM_IMPL(CLASS, NAME, VALUES) \
+    SMART_ENUM_IMPL_WITH_VALUE_TUPLES(CLASS, NAME, SMART_ENUM_IMPL_ARGS_TO_TUPLES(VALUES))
+
+#define SMART_ENUM_IMPL_WITH_VALUE_TUPLES(CLASS, NAME, VALUES) \
     enum CLASS \
         BOOST_PP_IIF(BOOST_VMD_IS_TUPLE(NAME), SMART_ENUM_IMPL_NAME_SIZE(NAME), NAME) \
     { \
@@ -40,7 +44,7 @@
 #define SMART_ENUM_IMPL_DESCRIPTION(NAME, VALUES) \
     SMART_ENUM_IMPL_DESCRIPTION_1( \
         BOOST_PP_TUPLE_ELEM(0, SMART_ENUM_IMPL_ARG_TO_TUPLE(NAME)), \
-        BOOST_PP_TUPLE_TO_SEQ(SMART_ENUM_IMPL_ARGS_TO_TUPLES(VALUES)) \
+        BOOST_PP_TUPLE_TO_SEQ(VALUES) \
     )
 
 #define SMART_ENUM_IMPL_DESCRIPTION_1(NAME, VALUES) \
@@ -52,15 +56,7 @@
 #define SMART_ENUM_IMPL_DESCRIPTION_2(_, NAME, VALUE) \
     value == NAME::BOOST_PP_TUPLE_ELEM(0, VALUE) ? \
         smart_enum::get_value_or_default \
-            BOOST_PP_TUPLE_POP_FRONT( \
-                BOOST_PP_TUPLE_INSERT(VALUE, 1, BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, VALUE))) \
-            ) :
-
-//         (a), 1 => (1)
-//      (a, 1), 2 => (2, 1)
-// (a, 1, "x"), 3 => (3, 1, "x")
-#define SMART_ENUM_IMPL_ENUM_VALUE_ARGS(VALUE, DEFAULT_VALUE) \
-    BOOST_PP_TUPLE_POP_FRONT(BOOST_PP_TUPLE_INSERT(VALUE, 1, DEFAULT_VALUE))
+            BOOST_PP_TUPLE_REPLACE(VALUE, 0, BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, VALUE))) :
 
 // (a, (b, 1), (c, 2, "x")) =>
 //  a = smart_enum::get_value_or_default(0),
@@ -70,18 +66,19 @@
     BOOST_PP_ENUM( \
         BOOST_PP_TUPLE_SIZE(VALUES), \
         SMART_ENUM_IMPL_ENUM_VALUES_1, \
-        SMART_ENUM_IMPL_ARGS_TO_TUPLES(VALUES) \
+        VALUES \
     )
 
 #define SMART_ENUM_IMPL_ENUM_VALUES_1(_, INDEX, VALUES) \
     SMART_ENUM_IMPL_VALUE_HEAD(INDEX, VALUES) = \
         smart_enum::get_value_or_default \
             BOOST_PP_IF(INDEX, \
-                SMART_ENUM_IMPL_ENUM_VALUE_ARGS( \
+                BOOST_PP_TUPLE_REPLACE( \
                     BOOST_PP_TUPLE_ELEM(INDEX, VALUES), \
+                    0, \
                     SMART_ENUM_IMPL_VALUE_HEAD(BOOST_PP_DEC(INDEX), VALUES) + 1 \
                 ), \
-                SMART_ENUM_IMPL_ENUM_VALUE_ARGS(BOOST_PP_TUPLE_ELEM(INDEX, VALUES), 0) \
+                BOOST_PP_TUPLE_REPLACE(BOOST_PP_TUPLE_ELEM(INDEX, VALUES), 0, 0) \
             )
 
 // (name, size) => name : size
