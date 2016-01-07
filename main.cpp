fc61a5c82c2bad68cfa8786cb1ac1a10b49e3693
JF <jarda@jarda.info>
2016-01-07 22:43:04 +0100

    Start implementing enum_traits.

--- main.cpp
+++ main.cpp
@@ -27,17 +27,22 @@ SMART_ENUM_CLASS
 
 int main()
 {
+    using smart_enum::enum_traits;
+
     static_assert(e_1::a ==  0, "");
     static_assert(e_1::b == 10, "");
     static_assert(e_1::c == 11, "");
     static_assert(e_1::d ==  1, "");
     static_assert(e_1::e ==  5, "");
 
-    assert(!strcmp(enum_description(e_1::a), "a"));
-    assert(!strcmp(enum_description(e_1::b), "b"));
-    assert(!strcmp(enum_description(e_1::c), "ccc"));
-    assert(!strcmp(enum_description(e_1::d), "ddd"));
-    assert(!strcmp(enum_description(e_1::e), "eee"));
+    static_assert(enum_traits<e_1>::count == 5, "");
+    static_assert(enum_traits<e_2>::count == 3, "");
+
+    assert(!strcmp(enum_traits<e_1>::description(e_1::a), "a"));
+    assert(!strcmp(enum_traits<e_1>::description(e_1::b), "b"));
+    assert(!strcmp(enum_traits<e_1>::description(e_1::c), "ccc"));
+    assert(!strcmp(enum_traits<e_1>::description(e_1::d), "ddd"));
+    assert(!strcmp(enum_traits<e_1>::description(e_1::e), "eee"));
 
     static_assert(sizeof(e_2) == sizeof(short), "");
 
--- smart_enum.hpp
+++ smart_enum.hpp
@@ -5,7 +5,6 @@
 
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/stringize.hpp>
-#include <boost/preprocessor/tuple/insert.hpp>
 #include <boost/preprocessor/tuple/pop_front.hpp>
 #include <boost/preprocessor/tuple/replace.hpp>
 #include <boost/preprocessor/tuple/to_seq.hpp>
@@ -27,7 +26,7 @@
         SMART_ENUM_IMPL_ENUM_VALUES(VALUES) \
     }; \
     \
-    SMART_ENUM_IMPL_DESCRIPTION(NAME, VALUES)
+    SMART_ENUM_IMPL_TRAITS(NAME, VALUES)
 
 //     a  => (a)
 // (a, b) => (a, b)
@@ -41,21 +40,9 @@
 #define SMART_ENUM_IMPL_ARGS_TO_TUPLES_1(_, INDEX, ARGS) \
     SMART_ENUM_IMPL_ARG_TO_TUPLE(BOOST_PP_TUPLE_ELEM(INDEX, ARGS))
 
-#define SMART_ENUM_IMPL_DESCRIPTION(NAME, VALUES) \
-    SMART_ENUM_IMPL_DESCRIPTION_1( \
-        BOOST_PP_TUPLE_ELEM(0, SMART_ENUM_IMPL_ARG_TO_TUPLE(NAME)), \
-        BOOST_PP_TUPLE_TO_SEQ(VALUES) \
-    )
-
-#define SMART_ENUM_IMPL_DESCRIPTION_1(NAME, VALUES) \
-    constexpr const char *enum_description(NAME value) \
-    { \
-        return BOOST_PP_SEQ_FOR_EACH(SMART_ENUM_IMPL_DESCRIPTION_2, NAME, VALUES) ""; \
-    }
-
-#define SMART_ENUM_IMPL_DESCRIPTION_2(_, NAME, VALUE) \
+#define SMART_ENUM_IMPL_DESCRIPTION(_, NAME, VALUE) \
     value == NAME::BOOST_PP_TUPLE_ELEM(0, VALUE) ? \
-        smart_enum::get_value_or_default \
+        smart_enum::detail::get_value_or_default \
             BOOST_PP_TUPLE_REPLACE(VALUE, 0, BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, VALUE))) :
 
 // (a, (b, 1), (c, 2, "x")) =>
@@ -71,7 +58,7 @@
 
 #define SMART_ENUM_IMPL_ENUM_VALUES_1(_, INDEX, VALUES) \
     SMART_ENUM_IMPL_VALUE_HEAD(INDEX, VALUES) = \
-        smart_enum::get_value_or_default \
+        smart_enum::detail::get_value_or_default \
             BOOST_PP_IF(INDEX, \
                 BOOST_PP_TUPLE_REPLACE( \
                     BOOST_PP_TUPLE_ELEM(INDEX, VALUES), \
@@ -85,6 +72,28 @@
 #define SMART_ENUM_IMPL_NAME_SIZE(NAME_SIZE) \
     BOOST_PP_TUPLE_ELEM(0, NAME_SIZE) : BOOST_PP_TUPLE_ELEM(1, NAME_SIZE)
 
+#define SMART_ENUM_IMPL_TRAITS(NAME, VALUES) \
+    SMART_ENUM_IMPL_TRAITS_1( \
+        BOOST_PP_TUPLE_ELEM(0, SMART_ENUM_IMPL_ARG_TO_TUPLE(NAME)), \
+        VALUES \
+    )
+
+#define SMART_ENUM_IMPL_TRAITS_1(NAME, VALUES) \
+    namespace smart_enum \
+    { \
+        template<> struct enum_traits<NAME> \
+        { \
+            static constexpr std::size_t count = BOOST_PP_TUPLE_SIZE(VALUES); \
+            \
+            static constexpr const char *description(NAME value) \
+            { \
+                return BOOST_PP_SEQ_FOR_EACH( \
+                    SMART_ENUM_IMPL_DESCRIPTION, NAME, BOOST_PP_TUPLE_TO_SEQ(VALUES) \
+                ) ""; \
+            } \
+        }; \
+    }
+
 // 0, ((a, 0), (b, 1)) => a
 // 1, ((a, 0), (b, 1)) => b
 #define SMART_ENUM_IMPL_VALUE_HEAD(INDEX, VALUES) \
@@ -92,40 +101,51 @@
 
 namespace smart_enum
 {
-    // get_value_or_default(default_value [, value [, value...]])
-    //  => return 1st "value" compatible with "default_value" or "default_value" if no such value exists
     template
     <
         typename T
     >
-    constexpr T get_value_or_default(T default_value)
+    struct enum_traits
     {
-        return default_value;
-    }
+    };
 
-    template
-    <
-        typename T,
-        typename U,
-        typename... Args,
-        typename std::enable_if<std::is_convertible<U, T>::value, int>::type = 0
-    >
-    constexpr U get_value_or_default(T, U value, Args...)
+    namespace detail
     {
-        return value;
-    }
+        // get_value_or_default(default_value [, value [, value...]])
+        //  => return 1st "value" compatible with "default_value" or "default_value" if no such value exists
+        template
+        <
+            typename T
+        >
+        constexpr T get_value_or_default(T default_value)
+        {
+            return default_value;
+        }
 
-    template
-    <
-        typename T,
-        typename U,
-        typename... Args,
-        typename std::enable_if<!std::is_convertible<U, T>::value, int>::type = 0
-    >
-    constexpr auto get_value_or_default(T default_value, U, Args... args)
-        -> decltype(get_value_or_default(default_value, args...))
-    {
-        return get_value_or_default(default_value, args...);
+        template
+        <
+            typename T,
+            typename U,
+            typename... Args,
+            typename std::enable_if<std::is_convertible<U, T>::value, int>::type = 0
+        >
+        constexpr U get_value_or_default(T, U value, Args...)
+        {
+            return value;
+        }
+
+        template
+        <
+            typename T,
+            typename U,
+            typename... Args,
+            typename std::enable_if<!std::is_convertible<U, T>::value, int>::type = 0
+        >
+        constexpr auto get_value_or_default(T default_value, U, Args... args)
+            -> decltype(get_value_or_default(default_value, args...))
+        {
+            return get_value_or_default(default_value, args...);
+        }
     }
 }
 
