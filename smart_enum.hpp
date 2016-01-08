14e3df6bf12f186f1b2af962c9eff82a47be62ce
JF <jarda@jarda.info>
2016-01-08 19:19:27 +0100

    Add license.

--- smart_enum.hpp
+++ smart_enum.hpp
@@ -1,3 +1,9 @@
+
+//          Copyright Jarda Flieger 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef SMART_ENUM_HEADER_INCLUDED
 #define SMART_ENUM_HEADER_INCLUDED
 
