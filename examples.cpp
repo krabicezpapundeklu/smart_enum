14c2390d7c3fee6ebca1c38e94272d914ba9b2c3
JF <jarda@jarda.info>
2016-02-03 16:10:52 +0100

    Refactor.

--- examples.cpp
+++ examples.cpp
@@ -4,26 +4,42 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <iostream>
-
 #include "smart_enum.hpp"
 
+//SMART_ENUM_IMPL_ADDITIONAL_DATA(n_1::n_2::e_1_c, ((a), (b, 10), (c, (1, 2, 3)), (d, 20, (4, 5, 6))))
 
-SMART_ENUM(e_1, (e_1_a, e_1_b, e_1_c))
-SMART_ENUM(n_1, e_2, (e_2_a, (e_2_b, 10, (1, 2, "xxx")), e_2_c))
+/*
+SMART_ENUM_IMPL_MEMBER_DEFINITION((a))
+SMART_ENUM_IMPL_MEMBER_DEFINITION((b, 10))
+SMART_ENUM_IMPL_MEMBER_DEFINITION((c, (1, 2, 3)))
+SMART_ENUM_IMPL_MEMBER_DEFINITION((d, 20, (4, 5, 6)))
+*/
 
-SMART_ENUM_CLASS((n_1, n_2), e_3, (e_3_a, e_3_b, e_3_c))
-SMART_ENUM_CLASS((n_1, n_2), (e_4, short), (e_4_a, (e_4_b, 10), (e_4_c, (false))))
+//SMART_ENUM_IMPL_MEMBER_DEFINITIONS(((a), (b, 10), (c, (1, 2, 3)), (d, 20, (4, 5, 6))))
+
+/*
+SMART_ENUM_IMPL_MEMBER_FROM_STRING(n_1::n_2::e_1_c, (a))
+SMART_ENUM_IMPL_MEMBER_FROM_STRING(n_1::n_2::e_1_c, (b, 10))
+SMART_ENUM_IMPL_MEMBER_FROM_STRING(n_1::n_2::e_1_c, (c, (1, 2, 3)))
+SMART_ENUM_IMPL_MEMBER_FROM_STRING(n_1::n_2::e_1_c, (d, 20, (4, 5, 6)))
+*/
+
+//SMART_ENUM_IMPL_FROM_STRING(n_1::n_2::e_1_c, ((a), (b, 10), (c, (1, 2, 3)), (d, 20, (4, 5, 6))))
+
+/*
+SMART_ENUM_IMPL_MEMBER_TO_STRING(n_1::n_2::e_1_c, (a))
+SMART_ENUM_IMPL_MEMBER_TO_STRING(n_1::n_2::e_1_c, (b, 10))
+SMART_ENUM_IMPL_MEMBER_TO_STRING(n_1::n_2::e_1_c, (c, (1, 2, 3)))
+SMART_ENUM_IMPL_MEMBER_TO_STRING(n_1::n_2::e_1_c, (d, 20, (4, 5, 6)))
+*/
+
+//SMART_ENUM_IMPL_TO_STRING(n_1::n_2::e_1_c, ((a), (b, 10), (c, (1, 2, 3)), (d, 20, (4, 5, 6))))
+
+SMART_ENUM(e_1_a, (a, (b, 10), (c, (1, 2, 3)), (d, 20, (4, 5, 6))))
+SMART_ENUM(n_1, (e_1_a, short), (a, (b, 10), (c, (1, 2, 3)), (d, 20, (4, 5, 6))))
+SMART_ENUM((n_1, n_2), e_1_a, (a, (b, 10), (c, (1, 2, 3)), (d, 20, (4, 5, 6))))
 
 int main()
 {
-    using namespace smart_enum;
-
-    std::cout << enum_traits<e_1>::name << std::endl;
-    std::cout << enum_traits<e_1>::full_name << std::endl;
-
-    std::cout << enum_traits<n_1::n_2::e_4>::name << std::endl;
-    std::cout << enum_traits<n_1::n_2::e_4>::full_name << std::endl;
-
     return 0;
 }
