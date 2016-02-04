a6ab8087c668416da67005e805fb4ceff80949a1
JF <jarda@jarda.info>
2016-02-04 13:54:24 +0100

    Generate full name.

--- examples.cpp
+++ examples.cpp
@@ -35,9 +35,21 @@ SMART_ENUM_IMPL_MEMBER_TO_STRING(n_1::n_2::e_1_c, (d, 20, (4, 5, 6)))
 
 // SMART_ENUM_IMPL_TO_STRING(n_1::n_2::e_1_c, ((a), (b, 10), (c, (1, 2, 3)), (d, 20, (4, 5, 6))))
 
+/*
 SMART_ENUM(e_1, (e_1_a, (e_1_b, 10), (e_1_c, (1, 2, 3)), (e_1_d, 20, (4, 5, 6))))
 SMART_ENUM(n_1, (e_2, short), (e_2_a, (e_2_b, 10), (e_2_c, (1, 2, 3)), (e_2_d, 20, (4, 5, 6))))
 SMART_ENUM((n_1, n_2), e_3, (e_3_a, (e_3_b, 10), (e_3_c, (1, 2, 3)), (e_3_d, 20, (4, 5, 6))))
+*/
+
+/*
+SMART_ENUM_IMPL_FULL_NAME(_, e_1)
+SMART_ENUM_IMPL_FULL_NAME((n_1), e_1)
+SMART_ENUM_IMPL_FULL_NAME((n_1, n_2), e_1)
+*/
+
+SMART_ENUM_IMPL_FULL_NAME_STRING(_, e_1)
+SMART_ENUM_IMPL_FULL_NAME_STRING((n_1), e_1)
+SMART_ENUM_IMPL_FULL_NAME_STRING((n_1, n_2), e_1)
 
 int main()
 {
